32175e9f4161eb5e03197502414041122e1929ff
liujeayat <liujeayat@gmail.com>
2020-04-13 20:19:11 +0900

    2

--- src/chat2/MultiServer.java
+++ src/chat2/MultiServer.java
@@ -10,8 +10,12 @@ public class MultiServer {
 
 	public static void main(String[] args) {
 
-		ServerSocket serverSocket = null;
-		Socket socket = null;
+		ServerSocket serverSocket = null;//서버를 개발할때 사용
+		//ServerSocket(int port) : 지정된 포트번호에 바인딩된 서버소켓을 만든다.
+		//ServerSocket(int port, int blocking) : 대기중인 최대 연결수를 추가지정한다.
+		
+		
+		Socket socket = null;//서버로 접속하거나 accept하는데 사용
 		PrintWriter out = null;
 		BufferedReader in = null;
 		String s = "";//클라이언트의 메시지를 저장
@@ -24,15 +28,22 @@ public class MultiServer {
 			//.....접속대기중.....
 			
 			//클라이언트가 접속 요청을 하면  accept()를 통해 받아들인다.
+			//객체가 반환이 되면(연결이 되면) InputStream을통해 받은 데이터를 읽을수 있다.
 			socket = serverSocket.accept();
 			
 			
 			
 			//서버->클라이언트 에게 메시지를 전송(출력)하기위한 스트림 생성
 			out = new PrintWriter(socket.getOutputStream(), true);
+			
+			
 			//클라이언트로 부터 메시지를 받기 위한 스트림 생성
-			in = new BufferedReader(
-					new InputStreamReader(socket.getInputStream()));
+			//InputStream 은 데이터를 byte로 읽기 때문에 상위 레벨의 데이터로 읽으려면
+			//InputStreamReader 로 읽어준다.
+			InputStreamReader inputStream = new InputStreamReader(socket.getInputStream());
+			
+			//BufferedReader에 InputStream을 래핑하여 데이터를 String으로 읽어온다.
+			in = new BufferedReader(inputStream);
 			
 			/*
 			클라이언트가 서버로 전송하는 최초의 메시지는 "대화명"이므로
@@ -56,8 +67,8 @@ public class MultiServer {
 				}
 				System.out.println(name+" ==> "+s);
 				out.println(">"+ name + "==>"+ s);
+				//PrintWriter out 의 객채변수임
 			}
-			
 			System.out.println("byeeeeee");
 			
 		} catch (Exception e) {
